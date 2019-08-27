3fe91440b204724b40eff4cdef16706d329bac6b
Marcin Jaworski <m.a.jaworski96@gmail.com>
2019-08-27 20:37:54 +0200

    Db script coma fix

--- db/V1.1.0_BasicData.sql
+++ db/V1.1.0_BasicData.sql
@@ -8,7 +8,7 @@ INSERT INTO states("name") VALUES
     ('archived');
 INSERT INTO public.users (username, password, email) VALUES
     ('user', '$2a$10$.BR9gtXEQFZsVxQCFXgavOvTfkyTjDYqBtyzNKIpwvxdDI2tuEooa', 'user@example.com'),
-    ('admin',,'$2a$10$w6j7reyRSZzBps0s23CZgeYYg2QlSjNUDxzwPxBiSqcWPNWWQMDge', 'admin@example.com');
+    ('admin','$2a$10$w6j7reyRSZzBps0s23CZgeYYg2QlSjNUDxzwPxBiSqcWPNWWQMDge', 'admin@example.com');
 INSERT INTO user_roles(user_id, role_id) VALUES
     (1, 1),
     (2, 1),
