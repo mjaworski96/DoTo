492803a903348fd4cb96b225097a380ee1e16b87
Marcin Jaworski <m.a.jaworski96@gmail.com>
2019-09-20 22:02:49 +0200

    Fix order on users list

--- backend/src/main/java/org/mjaworski/backend/persistance/repository/impl/jpa/UserJpaRepository.java
+++ backend/src/main/java/org/mjaworski/backend/persistance/repository/impl/jpa/UserJpaRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface UserJpaRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsernameIgnoreCase(String username);
 
-    @Query("SELECT u FROM User u")
+    @Query("SELECT u FROM User u ORDER BY u.id")
     List<User> get(Pageable pageable);
 
     @Query("SELECT COUNT(u) FROM User u")
